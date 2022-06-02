685780aaa580aabf93858a6ebfb0d1f666138de9
lkct <liangkct@yahoo.com>
2022-06-02 17:45:05 +0100

    performance test

---
+++ tests/test_performance.py
@@ -0,0 +1,59 @@
+from spikeinterface.extractors import MdaRecordingExtractor
+
+from data_utils import download_large, str2Path
+from run_hs2 import run_hs2
+import spikeinterface.sorters as ss
+
+from spikeinterface.sortingcomponents.peak_detection import detect_peaks
+
+import timeit
+import sys
+
+
+def test_performance(data_fn: str = 'sub-MEAREC-250neuron-Neuropixels_ecephys.mda') -> None:
+    data_path = str2Path(data_fn)
+    if not (data_path / 'raw.mda').exists():
+        download_large(data_fn)
+
+    recording = MdaRecordingExtractor(data_path)
+    assert recording.get_sampling_frequency() == 32000 and \
+        recording.get_num_samples() == 19200000
+    recording = recording.frame_slice(start_frame=0, end_frame=192000)
+
+    def timeit1(stmt): return timeit.timeit(stmt, number=1)
+
+    sihs_path = str2Path('results_HS')
+    hs2det_path = str2Path('result_HS2')
+
+    stdout, stderr = sys.stdout, sys.stderr
+    sys.stdout = sys.stderr = open('/dev/null', 'w')
+    try:
+        t_sihs = timeit1(lambda: ss.run_herdingspikes(
+            recording, filter=False, output_folder=sihs_path, remove_existing_folder=False, with_output=False))
+        t_hs2det = timeit1(lambda: run_hs2(
+            recording, filter=False, output_folder=hs2det_path))
+
+        t_peak = timeit1(lambda: detect_peaks(
+            recording, method='by_channel',
+            peak_sign='neg', detect_threshold=5, n_shifts=2,
+            chunk_size=10000, verbose=1, progress_bar=False))
+        t_peakloc = timeit1(lambda: detect_peaks(
+            recording, method='locally_exclusive',
+            peak_sign='neg', detect_threshold=5, n_shifts=2,
+            chunk_size=10000, verbose=1, progress_bar=False,
+            localization_dict=dict(method='center_of_mass', local_radius_um=150,
+                                   ms_before=0.1, ms_after=0.3)))
+    except Exception as e:
+        sys.stdout, sys.stderr = stdout, stderr
+        raise e
+    else:
+        sys.stdout, sys.stderr = stdout, stderr
+
+    print(t_sihs)
+    print(t_hs2det)
+    print(t_peak)
+    print(t_peakloc)
+
+
+if __name__ == '__main__':
+    test_performance()
