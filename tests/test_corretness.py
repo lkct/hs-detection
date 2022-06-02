638614998f1161f923784cc35e9f3a8b25e82ef9
lkct <liangkct@yahoo.com>
2022-06-02 01:12:15 +0100

    cleaner test output

--- tests/test_corretness.py
+++ tests/test_corretness.py
@@ -1,4 +1,5 @@
 import filecmp
+import sys
 
 import spikeinterface.sorters as ss
 from spikeinterface.extractors import MEArecRecordingExtractor
@@ -14,8 +15,16 @@ def test_corectness(data_fn: str = 'mearec_test_10s.h5'):
 
     recording = MEArecRecordingExtractor(data_path)
     # TODO: test contents of metadata
-    ss.run_herdingspikes(recording, output_folder=str2Path('results_HS'))
-    run_hs2(recording, output_folder=str2Path('result_HS2'))
+    stdout, stderr = sys.stdout, sys.stderr
+    sys.stdout = sys.stderr = open('/dev/null', 'w')
+    try:
+        ss.run_herdingspikes(recording, output_folder=str2Path('results_HS'))
+        run_hs2(recording, output_folder=str2Path('result_HS2'))
+    except Exception as e:
+        sys.stdout, sys.stderr = stdout, stderr
+        raise e
+    else:
+        sys.stdout, sys.stderr = stdout, stderr
     assert filecmp.cmp(str(str2Path('results_HS/HS2_detected.bin')),
                        str(str2Path('result_HS2/HS2_detected.bin')))
 
