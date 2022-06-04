c200cc3ba23800053ff6a6ed1ee30926d2344e31
lkct <liangkct@yahoo.com>
2022-06-04 19:58:49 +0100

    merge detect func

--- myherdingspikes/detection_localisation/detect.py
+++ myherdingspikes/detection_localisation/detect.py
@@ -127,44 +127,8 @@ class HS2Detection(object):
         else:
             num_frames = self.num_frames
 
-        self._detect(
-            file_name=str.encode(self.out_file_name[:-4]),
-            to_localize=self.to_localize,
-            sf=int(self.fps),
-            thres=self.threshold,
-            cutout_start=self.cutout_start,
-            cutout_end=self.cutout_end,
-            maa=self.maa,
-            maxsl=self.maxsl,
-            minsl=self.minsl,
-            ahpthr=self.ahpthr,
-            num_com_centers=self.num_com_centers,
-            decay_filtering=self.decay_filtering,
-            verbose=self.save_all,
-            num_frames=num_frames,
-            t_inc=t_inc,
-        )
-
-    def _detect(self,
-                file_name: bytes,
-                to_localize: bool,
-                sf: int,
-                thres: int,
-                cutout_start: int,
-                cutout_end: int,
-                maa: int,
-                maxsl: int,
-                minsl: int,
-                ahpthr: int,
-                num_com_centers: int,
-                decay_filtering: bool,
-                verbose: bool,
-                num_frames: int,
-                t_inc: int
-                ) -> None:
-
         # READ PROBE PARAMETERS
-        sf = int(sf)  # ensure sampling rate is integer, assumed to be in Hertz
+        sf = int(self.fps)  # ensure sampling rate is integer, assumed to be in Hertz
         num_channels = int(self.num_channels)
         spike_peak_duration = int(self.spike_peak_duration)
         noise_duration = int(self.noise_duration)
@@ -184,24 +148,24 @@ class HS2Detection(object):
             masked_channels[channel] = 0
 
         print(f'# Sampling rate: {sf}')
-        if to_localize:
+        if self.to_localize:
             print('# Localization On')
         else:
             print('# Localization Off')
-        if verbose:
+        if self.save_all:
             print('# Writing out extended detection info')
         print(f'# Number of recorded channels: {num_channels}')
         if num_channels < 20:
             print('# Few recording channels: not subtracing mean from activity')
         print(f'# Analysing frames: {num_frames}; Seconds: {num_frames / sf}')
-        print(f'# Frames before spike in cutout: {cutout_start}')
-        print(f'# Frames after spike in cutout: {cutout_end}')
+        print(f'# Frames before spike in cutout: {self.cutout_start}')
+        print(f'# Frames after spike in cutout: {self.cutout_end}')
 
         det: cython.pointer(Detection) = new Detection()
 
         # set tCut, tCut2 and tInc
-        t_cut_l = cutout_start + maxsl
-        t_cnt_r = cutout_end + maxsl
+        t_cut_l = self.cutout_start + self.maxsl
+        t_cnt_r = self.cutout_end + self.maxsl
         print(f'# tcuts: {t_cut_l} {t_cnt_r}')
 
         # cap at specified number of frames
@@ -222,19 +186,19 @@ class HS2Detection(object):
             self.neighbors, dtype=np.intc)
 
         det.SetInitialParams(cython.address(position_matrix[0, 0]), cython.address(neighbor_matrix[0, 0]), num_channels,
-                             spike_peak_duration, file_name, noise_duration,
+                             spike_peak_duration, self.out_file_name[:-4].encode(), noise_duration,
                              noise_amp_percent, inner_radius, cython.address(
             masked_channels[0]),
-            max_neighbors, num_com_centers, to_localize,
-            thres, cutout_start, cutout_end, maa, ahpthr, maxsl,
-            minsl, decay_filtering, verbose)
+            max_neighbors, self.num_com_centers, self.to_localize,
+            self.threshold, self.cutout_start, self.cutout_end, self.maa, self.ahpthr, self.maxsl,
+            self.minsl, self.decay_filtering, self.save_all)
 
         startTime = datetime.now()
         t0 = 0
         max_frames_processed = t_inc
         while t0 + t_inc + t_cnt_r <= num_frames:
             t1 = t0 + t_inc
-            if verbose:
+            if self.save_all:
                 print(f'# Analysing frames from {t0 - t_cut_l} to {t1 + t_cnt_r} '
                       f' ({100 * t0 / num_frames:.1f}%)')
 
@@ -256,15 +220,15 @@ class HS2Detection(object):
             'Probe Name': self.__class__.__name__,
             'Probe Object': self,
             'Date and Time Detection': str(now),
-            'Threshold': thres,
-            'Localization': to_localize,
+            'Threshold': self.threshold,
+            'Localization': self.to_localize,
             'Masked Channels': masked_channel_list,
-            'Associated Results File': file_name,
-            'Cutout Length': cutout_start + cutout_end,
+            'Associated Results File': self.out_file_name,
+            'Cutout Length': self.cutout_start + self.cutout_end,
             'Advice': 'For more information about detection, load and look at the parameters of the probe object',
         }
 
-        with open(f'{file_name.decode()}DetectionDict{now.strftime("%Y-%m-%d_%H%M%S_%f")}.txt', 'a') as f:
+        with open(f'{self.out_file_name[:-4]}DetectionDict{now.strftime("%Y-%m-%d_%H%M%S_%f")}.txt', 'a') as f:
             f.write(pprint.pformat(detection_state_dict))
 
         det.FinishDetection()
