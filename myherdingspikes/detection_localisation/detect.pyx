1f8c88aad011830de2858ead0853a93d4eecdc8d
lkct <liangkct@yahoo.com>
2022-06-04 14:13:33 +0100

    extract pyx extern

--- myherdingspikes/detection_localisation/detect.pyx
+++ myherdingspikes/detection_localisation/detect.pyx
@@ -1,32 +1,12 @@
 # distutils: language=c++
 
-import cython
 import numpy as np
 cimport numpy as np
-cimport cython
-import h5py
-from ctypes import CDLL
 import ctypes
 from datetime import datetime
-from libcpp cimport bool
-from libcpp.string cimport string
-import sys
 import pprint
-import warnings
+from SpkDonline cimport Detection
 
-cdef extern from "SpkDonline.h" namespace "SpkDonline":
-    cdef cppclass Detection:
-        Detection() except +
-        void InitDetection(long nFrames, int sf, int NCh, long ti, long int * Indices, int agl)
-        void SetInitialParams(int * pos_mtx, int * neigh_mtx, int num_channels,
-                              int spike_peak_duration, string file_name, int noise_duration,
-                              float noise_amp_percent, float inner_radius, int* _masked_channels, \
-                              int max_neighbors, int num_com_centers, bool to_localize, int thres, int cutout_start, int cutout_end, \
-                              int maa, int ahpthr, int maxsl, int minsl, bool decay_filtering, bool verbose)
-        void MedianVoltage(short * vm)
-        void MeanVoltage(short * vm, int tInc, int tCut)
-        void Iterate(short *vm, long t0, int tInc, int tCut, int tCut2, int maxFramesProcessed)
-        void FinishDetection()
 
 def detectData(probe, file_name, to_localize, sf, thres,
                cutout_start, cutout_end,
