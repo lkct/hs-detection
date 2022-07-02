7ac07452547ece4585c9d519a07aad869af2fbdc
lkct <liangkct@yahoo.com>
2022-07-02 19:36:31 +0100

    adjust cython

--- hs_detection/detect/detect.py
+++ hs_detection/detect/detect.py
@@ -33,7 +33,7 @@ class HSDetection(object):
 
     num_channels: int = cython.declare(int32_t)  # type: ignore
     chunk_size: int = cython.declare(int32_t)  # type: ignore
-    t_cut: int = cython.declare(int32_t)  # type: ignore
+    left_margin: int = cython.declare(int32_t)  # type: ignore
 
     rescale: bool = cython.declare(bool_t)  # type: ignore
     scale: NDArray[np.single] = cython.declare(np.ndarray)  # type: ignore
@@ -42,26 +42,26 @@ class HSDetection(object):
     median_reference: bool = cython.declare(bool_t)  # type: ignore
     average_reference: bool = cython.declare(bool_t)  # type: ignore
 
-    maxsl: int = cython.declare(int32_t)  # type: ignore
-    minsl: int = cython.declare(int32_t)  # type: ignore
+    spike_duration: int = cython.declare(int32_t)  # type: ignore
+    amp_avg_duration: int = cython.declare(int32_t)  # type: ignore
     threshold: float = cython.declare(single)  # type: ignore
-    maa: float = cython.declare(single)  # type: ignore
-    ahpthr: float = cython.declare(single)  # type: ignore
+    min_avg_amp: float = cython.declare(single)  # type: ignore
+    max_AHP_amp: float = cython.declare(single)  # type: ignore
 
     positions: NDArray[np.single] = cython.declare(np.ndarray)  # type: ignore
     neighbor_radius: float = cython.declare(single)  # type: ignore
     inner_radius: float = cython.declare(single)  # type: ignore
 
     noise_duration: int = cython.declare(int32_t)  # type: ignore
-    spike_peak_duration: int = cython.declare(int32_t)  # type: ignore
+    rise_duration: int = cython.declare(int32_t)  # type: ignore
 
     decay_filtering: bool = cython.declare(bool_t)  # type: ignore
-    noise_amp_percent: float = cython.declare(single)  # type: ignore
+    decay_ratio: float = cython.declare(single)  # type: ignore
 
     localize: bool = cython.declare(bool_t)  # type: ignore
 
     save_shape: bool = cython.declare(bool_t)  # type: ignore
-    out_file: Optional[Path] = cython.declare(object)  # type: ignore
+    shape_file: Optional[Path] = cython.declare(object)  # type: ignore
     cutout_start: int = cython.declare(int32_t)  # type: ignore
     cutout_end: int = cython.declare(int32_t)  # type: ignore
     cutout_length: int = cython.declare(int32_t)  # type: ignore
@@ -69,10 +69,9 @@ class HSDetection(object):
     verbose: bool = cython.declare(bool_t)  # type: ignore
 
     @cython.locals(recording=object, params=object,
-                   fps=single, float_param=single,
-                   num_channels=object, common_reference=object, out_file=object,
-                   l=np.ndarray, m=np.ndarray, r=np.ndarray,
-                   positions=np.ndarray)
+                   fps=single, l=np.ndarray, m=np.ndarray, r=np.ndarray,
+                   common_reference=str, duration_float=single,
+                   positions=np.ndarray, shape_file=object)
     def __init__(self, recording: Recording, params: Params) -> None:
         self.recording = recording
         self.num_segments = recording.get_num_segments()
@@ -80,7 +79,7 @@ class HSDetection(object):
                            for seg in range(self.num_segments)]
         fps = recording.get_sampling_frequency()
 
-        self.num_channels = num_channels = recording.get_num_channels()
+        self.num_channels = recording.get_num_channels()
         self.chunk_size = params['chunk_size']
 
         self.rescale = params['rescale']
@@ -98,9 +97,9 @@ class HSDetection(object):
                 -m * self.scale, dtype=np.single)
         else:
             self.scale: NDArray[np.single] = np.ones(
-                num_channels, dtype=np.single)
+                self.num_channels, dtype=np.single)
             self.offset: NDArray[np.single] = np.zeros(
-                num_channels, dtype=np.single)
+                self.num_channels, dtype=np.single)
 
         common_reference = params['common_reference']
         self.median_reference = common_reference == 'median'
@@ -109,17 +108,17 @@ class HSDetection(object):
             warnings.warn(
                 f'Number of channels too few for common {common_reference} reference')
 
-        float_param = params['spk_evaluation_time']
-        self.maxsl = int(float_param * fps / 1000 + 0.5)
-        float_param = params['amp_evaluation_time']
-        self.minsl = int(float_param * fps / 1000 + 0.5)
+        duration_float = params['spk_evaluation_time']
+        self.spike_duration = int(duration_float * fps / 1000 + 0.5)
+        duration_float = params['amp_evaluation_time']
+        self.amp_avg_duration = int(duration_float * fps / 1000 + 0.5)
         self.threshold = params['threshold']
-        self.maa = params['maa']
-        self.ahpthr = params['ahpthr']  # TODO:??? should be negative
-        self.maxsl -= 1  # TODO:??? -1
-        self.minsl -= 1
+        self.min_avg_amp = params['maa']
+        self.max_AHP_amp = params['ahpthr']  # TODO:??? should be negative
+        self.spike_duration -= 1  # TODO:??? -1
+        self.amp_avg_duration -= 1
         self.threshold /= 2  # TODO:??? /2
-        self.maa /= 2
+        self.min_avg_amp /= 2
 
         positions: NDArray[np.single] = np.array(
             [recording.get_channel_property(ch, 'location')
@@ -136,35 +135,35 @@ class HSDetection(object):
         self.inner_radius = params['inner_radius']
         self.inner_radius += RADIUS_EPS
 
-        float_param = params['peak_jitter']
-        self.noise_duration = int(float_param * fps / 1000 + 0.5)
-        float_param = params['event_length']
-        self.spike_peak_duration = int(float_param * fps / 1000 + 0.5)
+        duration_float = params['peak_jitter']
+        self.noise_duration = int(duration_float * fps / 1000 + 0.5)
+        duration_float = params['event_length']
+        self.rise_duration = int(duration_float * fps / 1000 + 0.5)
 
         self.decay_filtering = params['decay_filtering']
-        self.noise_amp_percent = params['noise_amp_percent']
+        self.decay_ratio = params['noise_amp_percent']
 
         self.localize = params['localize']
 
         self.save_shape = params['save_shape']
         if self.save_shape:
-            out_file = params['out_file']
-            if isinstance(out_file, str):
-                out_file = Path(out_file)
-            out_file.parent.mkdir(parents=True, exist_ok=True)
-            if out_file.suffix != '.bin':
-                out_file = out_file.with_suffix('.bin')
-            self.out_file = out_file
+            shape_file = params['out_file']
+            if isinstance(shape_file, str):
+                shape_file = Path(shape_file)
+            shape_file.parent.mkdir(parents=True, exist_ok=True)
+            if shape_file.suffix != '.bin':
+                shape_file = shape_file.with_suffix('.bin')
+            self.shape_file = shape_file
         else:
-            self.out_file = None
-        float_param = params['left_cutout_time']
-        self.cutout_start = int(float_param * fps / 1000 + 0.5)
-        float_param = params['right_cutout_time']
-        self.cutout_end = int(float_param * fps / 1000 + 0.5)
+            self.shape_file = None
+        duration_float = params['left_cutout_time']
+        self.cutout_start = int(duration_float * fps / 1000 + 0.5)
+        duration_float = params['right_cutout_time']
+        self.cutout_end = int(duration_float * fps / 1000 + 0.5)
         self.cutout_length = self.cutout_start + 1 + self.cutout_end
 
-        self.t_cut = self.noise_duration + max(self.cutout_length,
-                                               self.spike_peak_duration + 1 + self.maxsl)
+        self.left_margin = self.noise_duration + \
+            max(self.cutout_length, self.rise_duration + 1 + self.spike_duration)
 
         self.verbose = params['verbose']
 
@@ -190,21 +189,21 @@ class HSDetection(object):
 
     @cython.cfunc
     @cython.locals(segment_index=int32_t, start_frame=int32_t, end_frame=int32_t,
-                   lpad=int32_t, rpad=int32_t,
+                   pad_left=int32_t, pad_right=int32_t,
                    traces=np.ndarray, traces_float=np.ndarray)
     @cython.returns(np.ndarray)
     def get_traces(self, segment_index: int, start_frame: int, end_frame: int) -> NDArray[np.single]:
         if start_frame < 0:
-            lpad = -start_frame * self.num_channels
+            pad_left = -start_frame * self.num_channels
             start_frame = 0
         else:
-            lpad = 0
+            pad_left = 0
         if end_frame > self.num_frames[segment_index]:
-            rpad = (
-                end_frame - self.num_frames[segment_index]) * self.num_channels
+            pad_right = (end_frame - self.num_frames[segment_index]
+                         ) * self.num_channels
             end_frame = self.num_frames[segment_index]
         else:
-            rpad = 0
+            pad_right = 0
 
         traces: RealArray = self.recording.get_traces(
             segment_index=segment_index, start_frame=start_frame, end_frame=end_frame)
@@ -212,106 +211,101 @@ class HSDetection(object):
         traces_float: NDArray[np.single] = traces.astype(
             np.single, copy=False).reshape(-1)
 
-        if lpad or rpad:
-            traces_float = np.pad(traces_float, (lpad, rpad),
+        if pad_left or pad_right:
+            traces_float = np.pad(traces_float, (pad_left, pad_right),
                                   mode='constant', constant_values=0)
 
         return traces_float
 
     @cython.ccall
-    @cython.returns(object)
+    @cython.returns(list)
     def detect(self) -> Sequence[Mapping[str, RealArray]]:
         return [self.detect_seg(seg) for seg in range(self.num_segments)]
 
     @cython.cfunc
     @cython.locals(segment_index=int32_t,
-                   t_cut=int32_t, t_cut2=int32_t, t_inc=int32_t,
-                   out_file=object,
-                   t0=int32_t, t1=int32_t, t_end=int32_t,
-                   vm=np.ndarray,
-                   channel_ind=np.ndarray, sample_ind=np.ndarray,
+                   trace=np.ndarray, shape_file=object,
+                   num_frames=int32_t, chunk_start=int32_t, chunk_len=int32_t,
+                   sample_ind=np.ndarray, channel_ind=np.ndarray,
                    amplitude=np.ndarray, location=np.ndarray,
-                   spikes=np.ndarray,
-                   result=dict)
-    @cython.returns(object)
+                   spikes=np.ndarray, result=dict)
+    @cython.returns(dict)
     def detect_seg(self, segment_index: int) -> Mapping[str, RealArray]:
-        t_cut = self.cutout_start + self.maxsl
-        t_cut2 = self.cutout_end + self.maxsl
-        t_end = self.num_frames[segment_index] - t_cut2
-        t_inc = min(self.chunk_size, t_end - t_cut)
-
-        out_file = None if self.out_file is None \
-            else self.out_file.with_stem(f'{self.out_file.stem}-{segment_index}')
+        shape_file = None if self.shape_file is None \
+            else self.shape_file.with_stem(f'{self.shape_file.stem}-{segment_index}')
 
         det = newDet(  # type: ignore
             self.num_channels,
-            t_inc,
-            self.t_cut,
+            self.chunk_size,
+            self.left_margin,
             self.rescale,
             cython.cast(p_single, self.scale.data),
             cython.cast(p_single, self.offset.data),
             self.median_reference,
             self.average_reference,
-            self.maxsl,
-            self.minsl,
+            self.spike_duration,
+            self.amp_avg_duration,
             self.threshold,
-            self.maa,
-            self.ahpthr,
+            self.min_avg_amp,
+            self.max_AHP_amp,
             cython.cast(p_single, self.positions.data),
             self.neighbor_radius,
             self.inner_radius,
             self.noise_duration,
-            self.spike_peak_duration,
+            self.rise_duration,
             self.decay_filtering,
-            self.noise_amp_percent,
+            self.decay_ratio,
             self.localize,
             self.save_shape,
-            str(out_file).encode(),
+            str(shape_file).encode(),
             self.cutout_start,
             self.cutout_end
         )
 
-        t0 = 0
-        while t0 + t_inc <= t_end:
-            t1 = t0 + t_inc
+        num_frames = self.num_frames[segment_index] \
+            - self.cutout_end - self.spike_duration  # TODO:??? no need to subtract this
+        chunk_start = 0
+        chunk_len = min(self.chunk_size, num_frames)
+        while chunk_start < num_frames:
+            chunk_len = min(chunk_len, num_frames - chunk_start)
+
             if self.verbose:
                 print(f'HSDetection: Analysing segment {segment_index}, '
-                      f'frames from {t0:8d} to {t1:8d} '
-                      f' ({100 * t0 / t_end:.1f}%)')
+                      f'frames from {chunk_start:8d} to {chunk_start + chunk_len:8d} '
+                      f' ({100 * chunk_start / num_frames:.1f}%)')
 
-            vm = self.get_traces(segment_index=segment_index,
-                                 start_frame=t0 - self.t_cut, end_frame=t1 + t_cut2)
-            det.step(cython.cast(p_single, vm.data), t0, t_inc)
+            trace = self.get_traces(segment_index=segment_index,
+                                    start_frame=chunk_start - self.left_margin,
+                                    end_frame=chunk_start + chunk_len)
+            det.step(cython.cast(p_single, trace.data), chunk_start, chunk_len)
 
-            t0 += t_inc
-            if t0 < t_end:
-                t_inc = min(t_inc, t_end - t0)
+            chunk_start += chunk_len
 
         det_len = det.finish()
-        det_res = det.getResult()
+        det_result = det.getResult()
 
-        channel_ind = np.empty(det_len, dtype=np.int32)
         sample_ind = np.empty(det_len, dtype=np.int32)
+        channel_ind = np.empty(det_len, dtype=np.int32)
         amplitude = np.empty(det_len, dtype=np.int32)
         location = np.empty((det_len, 2), dtype=np.single)
         for i in range(det_len):
-            sample_ind[i] = det_res[i].frame
-            channel_ind[i] = det_res[i].channel
-            amplitude[i] = det_res[i].amplitude
-            location[i, 0] = det_res[i].position.x
-            location[i, 1] = det_res[i].position.y
+            sample_ind[i] = det_result[i].frame
+            channel_ind[i] = det_result[i].channel
+            amplitude[i] = det_result[i].amplitude
+            location[i, 0] = det_result[i].position.x
+            location[i, 1] = det_result[i].position.y
 
         delDet(det)  # type: ignore
 
-        if out_file is not None and out_file.stat().st_size > 0:
+        if shape_file is not None and shape_file.stat().st_size > 0:
             spikes: NDArray[np.int16] = np.memmap(
-                str(out_file), dtype=np.int16, mode='r').reshape(-1, self.cutout_length)
+                str(shape_file), dtype=np.int16, mode='r').reshape(-1, self.cutout_length)
         else:
             spikes: NDArray[np.int16] = np.empty(
                 (0, self.cutout_length), dtype=np.int16)
 
-        result: dict[str, RealArray] = {'channel_ind': channel_ind,
-                                        'sample_ind': sample_ind,
+        result: dict[str, RealArray] = {'sample_ind': sample_ind,
+                                        'channel_ind': channel_ind,
                                         'amplitude': amplitude}
         if self.localize:
             result |= {'location': location}
